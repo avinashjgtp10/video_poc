26ba17991741cbc53ca13ba398bbcd7c7d731095
Avinash Balaso Jagtap <avinashjgtp10@gmail.com>
2020-06-01 20:46:15 +0530

    video poc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { VideoPlayer } from '@ionic-native/video-player/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    VideoPlayer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { VideoPlayer ,VideoOptions } from '@ionic-native/video-player/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
-}
+  videoOpts : VideoOptions ;
+  constructor(private videoPlayer : VideoPlayer) {
+  
+  }
+  
+  public playVideo(){
+    console.log("calling")
+      this.videoOpts = {volume : 1.0};
+      this.videoPlayer.play('https://www.youtube.com/watch?v=aqz-KE-bpKQ&t=109s').then(() => {
+      console.log('video completed');
+      }).catch(err => {
+      console.log(err);
+      });    
+  }
+  public stopPlayingVideo(){
+      this.videoPlayer.close();
+  }
+  
+  }
